429b908769b5bd34261cd1975c1d19ce76162886
18107 <18107@users.noreply.github.com>
2024-07-27 18:50:58 +0930

    Add files via upload

---
+++ FasterShards/CarryablesSocketPatch.cs
@@ -0,0 +1,30 @@
+﻿using CG.Ship.Hull;
+using HarmonyLib;
+using VoidManager.Utilities;
+
+namespace FasterShards
+{
+    [HarmonyPatch(typeof(CarryablesSocket))]
+    internal class CarryablesSocketPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("Start")]
+        static void Start(CarryablesSocket __instance)
+        {
+            if (__instance.name == "GalaxyMapTerminal_SingleSocket")
+            {
+                __instance.IsOutput = false;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("OnAcquireCarryablePassthrough")]
+        static void OnAcquireCarryablePassthrough(CarryablesSocket __instance)
+        {
+            if (__instance.name == "GalaxyMapTerminal_SingleSocket")
+            {
+                Tools.DelayDo(() => __instance.SetState(Gameplay.Carryables.SocketState.Open), 0);
+            }
+        }
+    }
+}
---
+++ FasterShards/VoidManagerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using VoidManager.MPModChecks;
+
+namespace FasterShards
+{
+    public class VoidManagerPlugin : VoidManager.VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.Host;
+
+        public override string Author => "18107";
+
+        public override string Description => "Makes data shards process faster";
+    }
+}
